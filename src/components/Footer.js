919d63005a5cbb67a35ff832c0201d1ceabeb9c9
Tiago Sciencia <tiago@bliive.com>
2020-01-11 21:58:37 -0300

    add instagram feed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import InstagramFeed from './InstagramFeed'
 import './Footer.css'
 
 export default ({ globalSettings, socialSettings, navLinks }) => (
-  <footer className='Footer'>
+  <div>
+    <h2 className="taCenter">
+      Follow us{' '}
+      <a href="https://instagram.com/beliive.br/" style={{textDecoration:"none"}} target="_blank">@beliive.br</a>
+    </h2>
+    <br />
+    <InstagramFeed count="8" />
+    <footer className='Footer'>
     <div className='container taCenter'>
       <span>Beliive © {new Date().getFullYear()} All rights reserved.</span>
     </div>
   </footer>
+  </div>
 )
