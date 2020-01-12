2cde6b848dbfb2b36c81250f78643545aad0285f
Tiago Sciencia <tiago@bliive.com>
2020-01-11 21:17:07 -0300

    update template

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -1,93 +1,93 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { MapPin, Smartphone, Mail } from 'react-feather'
+// import React from 'react'
+// import Helmet from 'react-helmet'
+// import { MapPin, Smartphone, Mail } from 'react-feather'
 
-import PageHeader from '../components/PageHeader'
-import FormSimpleAjax from '../components/FormSimpleAjax'
-import Content from '../components/Content'
-import './ContactPage.css'
+// import PageHeader from '../components/PageHeader'
+// import FormSimpleAjax from '../components/FormSimpleAjax'
+// import Content from '../components/Content'
+// import './ContactPage.css'
 
-// Export Template for use in CMS preview
-export const ContactPageTemplate = ({
-  body,
-  title,
-  subtitle,
-  featuredImage,
-  address,
-  phone,
-  email
-}) => (
-  <main className="Contact">
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
+// // Export Template for use in CMS preview
+// export const ContactPageTemplate = ({
+//   body,
+//   title,
+//   subtitle,
+//   featuredImage,
+//   address,
+//   phone,
+//   email
+// }) => (
+//   <main className="Contact">
+//     <Helmet>
+//       <title>{title}</title>
+//     </Helmet>
 
-    <PageHeader
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
+//     <PageHeader
+//       title={title}
+//       subtitle={subtitle}
+//       backgroundImage={featuredImage}
+//     />
 
-    <section className="section Contact--Section1">
-      <div className="container Contact--Section1--Container">
-        <div>
-          <Content source={body} />
+//     <section className="section Contact--Section1">
+//       <div className="container Contact--Section1--Container">
+//         <div>
+//           <Content source={body} />
 
-          <div className="Contact--Details">
-            {address && (
-              <a
-                className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
-                  address
-                )}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <MapPin /> {address}
-              </a>
-            )}
-            {phone && (
-              <a className="Contact--Details--Item" href={`tel:${phone}`}>
-                <Smartphone /> {phone}
-              </a>
-            )}
-            {email && (
-              <a className="Contact--Details--Item" href={`mailto:${email}`}>
-                <Mail /> {email}
-              </a>
-            )}
-          </div>
-        </div>
+//           <div className="Contact--Details">
+//             {address && (
+//               <a
+//                 className="Contact--Details--Item"
+//                 href={`https://www.google.com.au/maps/search/${encodeURI(
+//                   address
+//                 )}`}
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 <MapPin /> {address}
+//               </a>
+//             )}
+//             {phone && (
+//               <a className="Contact--Details--Item" href={`tel:${phone}`}>
+//                 <Smartphone /> {phone}
+//               </a>
+//             )}
+//             {email && (
+//               <a className="Contact--Details--Item" href={`mailto:${email}`}>
+//                 <Mail /> {email}
+//               </a>
+//             )}
+//           </div>
+//         </div>
 
-        <div>
-          <FormSimpleAjax name="Simple Form Ajax" />
-        </div>
-      </div>
-    </section>
-  </main>
-)
+//         <div>
+//           <FormSimpleAjax name="Simple Form Ajax" />
+//         </div>
+//       </div>
+//     </section>
+//   </main>
+// )
 
-const ContactPage = ({ data: { page } }) => (
-  <ContactPageTemplate {...page.frontmatter} body={page.html} />
-)
+// const ContactPage = ({ data: { page } }) => (
+//   <ContactPageTemplate {...page.frontmatter} body={page.html} />
+// )
 
-export default ContactPage
+// export default ContactPage
 
-export const pageQuery = graphql`
-  query ContactPage($id: String!) {
-    page: markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-        template
-        subtitle
-        featuredImage {
-          ...FluidImage
-        }
-        address
-        phone
-        email
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query ContactPage($id: String!) {
+//     page: markdownRemark(id: { eq: $id }) {
+//       html
+//       frontmatter {
+//         title
+//         template
+//         subtitle
+//         featuredImage {
+//           ...FluidImage
+//         }
+//         address
+//         phone
+//         email
+//       }
+//     }
+//   }
+// `
