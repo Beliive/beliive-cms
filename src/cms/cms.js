6ba66a6264306bc283ec11e0c308849f59dc0a3b
Tiago Sciencia <tiago@bliive.com>
2020-01-11 21:43:01 -0300

    update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,9 @@ import CMS from 'netlify-cms'
 import './cms-utils'
 
 import { HomePageTemplate } from '../templates/HomePage'
-// import { AboutPageTemplate } from '../templates/AboutPage'
-// import { ContactPageTemplate } from '../templates/ContactPage'
-// import { DefaultPageTemplate } from '../templates/DefaultPage'
+import { AboutPageTemplate } from '../templates/AboutPage'
+import { ContactPageTemplate } from '../templates/ContactPage'
+import { DefaultPageTemplate } from '../templates/DefaultPage'
 import { BlogIndexTemplate } from '../templates/BlogIndex'
 import { SinglePostTemplate } from '../templates/SinglePost'
 
@@ -14,15 +14,15 @@ CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('home-page', ({ entry }) => (
   <HomePageTemplate {...entry.toJS().data} />
 ))
-// CMS.registerPreviewTemplate('about-page', ({ entry }) => (
-//   <AboutPageTemplate {...entry.toJS().data} />
-// ))
-// CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
-//   <ContactPageTemplate {...entry.toJS().data} />
-// ))
-// CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
-//   <DefaultPageTemplate {...entry.toJS().data} />
-// ))
+CMS.registerPreviewTemplate('about-page', ({ entry }) => (
+  <AboutPageTemplate {...entry.toJS().data} />
+))
+CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
+  <ContactPageTemplate {...entry.toJS().data} />
+))
+CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
+  <DefaultPageTemplate {...entry.toJS().data} />
+))
 CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
   <BlogIndexTemplate {...entry.toJS().data} />
 ))
--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -1,51 +1,51 @@
-// import React from 'react'
-// import Helmet from 'react-helmet'
+import React from 'react'
+import Helmet from 'react-helmet'
 
-// import PageHeader from '../components/PageHeader'
-// import Content from '../components/Content'
+import PageHeader from '../components/PageHeader'
+import Content from '../components/Content'
 
-// // Export Template for use in CMS preview
-// export const DefaultPageTemplate = ({
-//   title,
-//   subtitle,
-//   featuredImage,
-//   body
-// }) => (
-//   <main className="DefaultPage">
-//     <Helmet>
-//       <title>{title}</title>
-//     </Helmet>
+// Export Template for use in CMS preview
+export const DefaultPageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
+  body
+}) => (
+  <main className="DefaultPage">
+    <Helmet>
+      <title>{title}</title>
+    </Helmet>
 
-//     <PageHeader
-//       title={title}
-//       subtitle={subtitle}
-//       backgroundImage={featuredImage}
-//     />
+    <PageHeader
+      title={title}
+      subtitle={subtitle}
+      backgroundImage={featuredImage}
+    />
 
-//     <section className="section">
-//       <div className="container">
-//         <Content source={body} />
-//       </div>
-//     </section>
-//   </main>
-// )
+    <section className="section">
+      <div className="container">
+        <Content source={body} />
+      </div>
+    </section>
+  </main>
+)
 
-// const DefaultPage = ({ data: { page } }) => (
-//   <DefaultPageTemplate {...page.frontmatter} body={page.html} />
-// )
-// export default DefaultPage
+const DefaultPage = ({ data: { page } }) => (
+  <DefaultPageTemplate {...page.frontmatter} body={page.html} />
+)
+export default DefaultPage
 
-// export const pageQuery = graphql`
-//   query DefaultPage($id: String!) {
-//     page: markdownRemark(id: { eq: $id }) {
-//       html
-//       frontmatter {
-//         title
-//         subtitle
-//         featuredImage {
-//           ...FluidImage
-//         }
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query DefaultPage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        subtitle
+        featuredImage {
+          ...FluidImage
+        }
+      }
+    }
+  }
+`
